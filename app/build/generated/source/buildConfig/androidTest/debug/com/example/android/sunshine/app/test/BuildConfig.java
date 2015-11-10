d88080edd34ca53c99d289f5fb30e70e2fe67060
eriDam <erika.c.caballero@gmail.com>
2015-11-10 01:15:42 +0100

    Fourth change, rename class and create inner class  AsyncTask

--- app/build/generated/source/buildConfig/androidTest/debug/com/example/android/sunshine/app/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/android/sunshine/app/test/BuildConfig.java
@@ -10,4 +10,6 @@ public final class BuildConfig {
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String OPEN_WEATHER_MAP_API_KEY = "8effa1e2a8ed0c324ebbe0a14b48491a";
 }

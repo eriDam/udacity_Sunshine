6d1370481ea8766ba16fbdbb7e4d22afa0d682d2
eriDam <erika.c.caballero@gmail.com>
2015-11-20 22:47:04 +0100

    Error solved with Share option

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -36,7 +36,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detailfragment, menu);
+        getMenuInflater().inflate(R.menu.detail, menu);
         return true;
     }
 

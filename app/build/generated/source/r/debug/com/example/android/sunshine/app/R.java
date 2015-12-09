3e51fc775b937699c182474562b32401534a1624
eriDam <erika.c.caballero@gmail.com>
2015-12-09 17:07:57 +0100

    5.02 Build today list Item

--- app/build/generated/source/r/debug/com/example/android/sunshine/app/R.java
+++ app/build/generated/source/r/debug/com/example/android/sunshine/app/R.java
@@ -2396,18 +2396,19 @@ containing a value of this type.
         public static final int fragment_detail=0x7f040028;
         public static final int fragment_main=0x7f040029;
         public static final int list_item_forecast=0x7f04002a;
-        public static final int notification_media_action=0x7f04002b;
-        public static final int notification_media_cancel_action=0x7f04002c;
-        public static final int notification_template_big_media=0x7f04002d;
-        public static final int notification_template_big_media_narrow=0x7f04002e;
-        public static final int notification_template_lines=0x7f04002f;
-        public static final int notification_template_media=0x7f040030;
-        public static final int notification_template_part_chronometer=0x7f040031;
-        public static final int notification_template_part_time=0x7f040032;
-        public static final int select_dialog_item_material=0x7f040033;
-        public static final int select_dialog_multichoice_material=0x7f040034;
-        public static final int select_dialog_singlechoice_material=0x7f040035;
-        public static final int support_simple_spinner_dropdown_item=0x7f040036;
+        public static final int list_item_forecast_today=0x7f04002b;
+        public static final int notification_media_action=0x7f04002c;
+        public static final int notification_media_cancel_action=0x7f04002d;
+        public static final int notification_template_big_media=0x7f04002e;
+        public static final int notification_template_big_media_narrow=0x7f04002f;
+        public static final int notification_template_lines=0x7f040030;
+        public static final int notification_template_media=0x7f040031;
+        public static final int notification_template_part_chronometer=0x7f040032;
+        public static final int notification_template_part_time=0x7f040033;
+        public static final int select_dialog_item_material=0x7f040034;
+        public static final int select_dialog_multichoice_material=0x7f040035;
+        public static final int select_dialog_singlechoice_material=0x7f040036;
+        public static final int support_simple_spinner_dropdown_item=0x7f040037;
     }
     public static final class menu {
         public static final int detail=0x7f0f0000;

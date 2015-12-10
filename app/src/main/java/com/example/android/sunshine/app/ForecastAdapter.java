d86fc3879fedeb3f33bef7db2cbcf9910cc0b146
eriDam <erika.c.caballero@gmail.com>
2015-12-10 18:21:25 +0100

    5.04 Add method newView to chose two view type in ForecastAdapter

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -19,16 +19,43 @@ public class ForecastAdapter extends CursorAdapter {
         super(context, c, flags);
     }
 
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
+
+    /**
+     * Copy/paste note: Replace existing newView() method in ForecastAdapter with this one.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+
+        //  Determine layoutId from viewType
+        switch (viewType) {
+                        case VIEW_TYPE_TODAY: {
+                                layoutId = R.layout.list_item_forecast_today;
+                                break;
+                            }
+                       case VIEW_TYPE_FUTURE_DAY: {
+                                layoutId = R.layout.list_item_forecast;
+                                break;
+                            }
+                    }
+                return LayoutInflater.from(context).inflate(layoutId, parent, false);
+    }
 
     /*
         Remember that these views are reused as needed.
      */
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
-        return view;
-    }
+//    @Override
+//    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+//        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+//
+//        return view;
+//    }
     /*
             This is where we fill-in the views with the contents of the cursor.
          */
@@ -68,4 +95,15 @@ public class ForecastAdapter extends CursorAdapter {
         TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
         lowView.setText(Utility.formatTemperature(low, isMetric));
     }
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
+
 }
